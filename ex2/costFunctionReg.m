0751d62b811f928adf63093077a8705c227566f9
Jinah Adam <jinahadam@gmail.com>
2017-08-19 23:08:16 +0800

    ex2 regularization

--- ex2/costFunctionReg.m
+++ ex2/costFunctionReg.m
@@ -4,21 +4,35 @@ function [J, grad] = costFunctionReg(theta, X, y, lambda)
 %   theta as the parameter for regularized logistic regression and the
 %   gradient of the cost w.r.t. to the parameters. 
 
-% Initialize some useful values
+
+
 m = length(y); % number of training examples
+n = size(X,2); % number of features
+h = zeros(m,1);
 
-% You need to return the following variables correctly 
 J = 0;
-grad = zeros(size(theta));
-
-% ====================== YOUR CODE HERE ======================
-% Instructions: Compute the cost of a particular choice of theta.
-%               You should set J to the cost.
-%               Compute the partial derivatives and set grad to the partial
-%               derivatives of the cost w.r.t. each parameter in theta
+grad = zeros(n,1);
 
 
+for i = 1:m
+    
+    d = X(i,:) * theta;
+    h(i) = 1.0/(1.0 + exp(-d));
+  
+end    
 
+
+t1 = theta(2:n,:);
+J = (-1.0/m) * sum(y .* log(h) + (1-y) .* log(1-h)) + (lambda/(2.0 * m)) * t1' * t1;
+
+for j = 1:n    
+    if (j == 1)
+      grad(j) = (1.0/m )* sum((h-y)' * X(:,j));
+    else 
+      grad(j) = (1.0/m )* sum((h-y)' * X(:,j)) + (lambda/m)* theta(j) ;
+    end
+    
+end    
 
 
 
--- ex2/predict.m
+++ ex2/predict.m
@@ -14,7 +14,6 @@ p = zeros(m, 1);
 %               your learned logistic regression parameters. 
 %               You should set p to a vector of 0's and 1's
 %
-X1 = [ones(size(X, 1), 1) X];
 
 for i = 1:m 
   if (sigmoid(X(i,:) * theta) >= 0.5)
